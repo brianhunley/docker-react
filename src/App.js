f1d508f66d251da89869092fcc42ad1572babfb5
Brian Hunley <brian.hunley@gmail.com>
2023-12-12 06:58:33 -0500

    Update to test full deployment to AWS.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>Welcome to React!</p>
+        <p>With another deployed update to AWS!</p>
         <a
           className='App-link'
           href='https://reactjs.org'
